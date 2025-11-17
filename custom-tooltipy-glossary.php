b512c9b01dc9ab0ceacd497c1ca07b9cecd7b0da
Mathieu Nogaret <mathieu.nogaret@gmail.com>
2025-11-17 10:45:04 +0100

    Fix

--- custom-tooltipy-glossary.php
+++ custom-tooltipy-glossary.php
@@ -46,14 +46,14 @@ function custom_tooltipy_glossary( $atts ) {
         $text_all = __('ALL','tooltipy-lang');
     }
 
-//    $current_letter_class = '';
-//    if ( empty ( $_GET['letter'] ) ) {
-//        $current_letter_class = 'bluet_glossary_current_letter';
-//    }
+    $current_letter_class = '';
+    if ( empty ( $_GET['letter'] ) ) {
+        $current_letter_class = 'bluet_glossary_current_letter';
+    }
 
-//    $all_link = get_permalink();
+    $all_link = get_permalink();
 
-//    $ret = "<div class='kttg_glossary_div'>";
+    $ret = "<div class='kttg_glossary_div'>";
 
 //    $ret. = '<div class="kttg_glossary_header"><span class="bluet_glossary_all ' . $current_letter_class . '"><a href=\'' . $all_link . '\'>' . $text_all . '</a></span> - ';
 
