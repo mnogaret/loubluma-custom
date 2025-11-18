9daa15fb98d90fdd40fd97aa4f6e54fff6631bd4
Mathieu Nogaret <mathieu.nogaret@gmail.com>
2025-11-18 17:37:17 +0100

    Patch tooltipy

---
+++ functions.php
@@ -0,0 +1,39 @@
+add_action('wp_enqueue_scripts', function () {
+    // Patch de tooltipy afin de ne pas supprimer certains espaces
+    // Voir https://wordpress.org/support/topic/whitespace-lost-after-keyword/
+    wp_add_inline_script('kttg-tooltips-functions-script', <<<'JS'
+(function() {
+    if (typeof window.findAndReplaceDOMText !== 'function') {
+        return;
+    }
+
+    var original = window.findAndReplaceDOMText;
+
+    window.findAndReplaceDOMText = function(node, options) {
+        if (options && typeof options.replace === 'function') {
+            var origReplace = options.replace;
+
+            options.replace = function() {
+                var portion = arguments[0];
+                var result  = origReplace.apply(this, arguments);
+
+                if (
+                    result === "" &&
+                    (portion.text === "" ||
+                     portion.text === " " ||
+                     portion.text === "\t" ||
+                     portion.text === "\n")
+                ) {
+                    return portion.text;
+                }
+
+                return result;
+            };
+        }
+
+        return original(node, options);
+    };
+})();
+JS
+    );
+}, 20);
