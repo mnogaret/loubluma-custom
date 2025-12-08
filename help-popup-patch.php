03495f948bedfbaad4954fb3b729752684305292
Mathieu Nogaret <mathieu.nogaret@gmail.com>
2025-12-08 16:37:18 +0100

    help

--- help-popup-patch.php
+++ help-popup-patch.php
@@ -1,56 +1,17 @@
 <?php
 
-add_action('wp_footer', function () {
-  if (!is_front_page()) return;
-  ?>
-  <div id="bleymard-help-banner">
-    Pour ouvrir le menu, appuyez sur le bouton ☰en haut de l’écran.
-    <span id="bleymard-help-close">✖</span>
-  </div>
-  <?php
-});
-
-add_action('wp_footer', function () {
-  ?>
-  <script>
-    (function () {
-      if (window.innerWidth > 768) return;
-
-      const banner = document.getElementById('bleymard-help-banner');
-      const closeBtn = document.getElementById('bleymard-help-close');
-
-      if (!banner || !closeBtn) return;
-
-      setTimeout(() => {
-        banner.style.display = 'block';
-      }, 3000);
-
-      closeBtn.addEventListener('click', () => {
-        banner.style.display = 'none';
-      });
-
-      setTimeout(() => {
-        banner.style.display = 'none';
-      }, 15000);
-    })();
-  </script>
-  <?php
-});
-
 function custom_loubluma_help( $atts, $content = null ) {
   $atts = shortcode_atts(
     [
-      'show' => 3,
       'hide' => 15
     ],
     $atts);
 
-  $show = (int) $atts['show'] * 1000;
   $hide = (int) $atts['hide'] * 1000;
 
   ob_start();
   ?>
-  <div id="bleymard-help-banner" data-show="<?php echo $show; ?>" data-hide="<?php echo $hide; ?>">
+  <div id="bleymard-help-banner" data-hide="<?php echo $hide; ?>">
     <?php echo wp_kses_post($content); ?>
     <span id="bleymard-help-close">✖</span>
   </div>
@@ -69,12 +30,7 @@ add_action('wp_footer', function () {
 
       if (!banner || !closeBtn) return;
 
-      const showDelay = parseInt(banner.dataset.show || '3000', 10);
       const hideDelay = parseInt(banner.dataset.hide || '15000', 10);
-
-      setTimeout(() => {
-        banner.style.display = 'block';
-      }, showDelay);
 
       closeBtn.addEventListener('click', () => {
         banner.style.display = 'none';
@@ -82,7 +38,7 @@ add_action('wp_footer', function () {
 
       setTimeout(() => {
         banner.style.display = 'none';
-      }, showDelay + hideDelay);
+      }, hideDelay);
     })();
   </script>
   <?php
