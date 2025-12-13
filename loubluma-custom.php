18040073e5e6083dc141191aede6caebbc135974
Mathieu Nogaret <mathieu.nogaret@gmail.com>
2025-12-13 09:50:20 +0100

    breakpoint responsive

--- loubluma-custom.php
+++ loubluma-custom.php
@@ -9,3 +9,8 @@
 require __DIR__ . '/tooltipy-glossary-patch.php';
 require __DIR__ . '/whitespace-patch.php';
 require __DIR__ . '/help-popup-patch.php';
+
+add_filter( 'astra_tablet_breakpoint', function () {
+    return 1030;
+});
+
